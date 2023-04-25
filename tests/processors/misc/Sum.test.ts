cd7e6fabf3d1880c2ae5c2709777ec7a697e027f
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-04-25 16:33:38 +0200

    Add test cases for processors

---
+++ tests/processors/misc/Sum.test.ts
@@ -0,0 +1,14 @@
+import { Environment, Identity } from "../../../src";
+import { Sum } from "../../../src/processors/misc/Sum";
+
+test('process', () => {
+    //given
+    const sum = new Sum();
+    const sourceModel = [1,2]
+    sum.environment.setProperty(Environment.ORIGINAL_MODEL, sourceModel);
+    sum.environment.setProperty(Environment.SOURCE_MODEL, sourceModel);
+    //when
+    sum.process()
+    //then
+    expect(sum.environment.getResult()).toBe(3)
+});
