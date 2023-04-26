1591086815708d4f3fed1b7fb3fc1a062f61f158
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-04-26 17:33:28 +0200

    Add test case for BFInterpreter

---
+++ tests/processors/misc/BFInterpreter.test.ts
@@ -0,0 +1,15 @@
+import { Environment, Identity, Rot13 } from "../../../src";
+import { Sum } from "../../../src/processors/misc/Sum";
+import {BFInterpreter} from "../../../src/processors/misc/BFInterpreter";
+
+test('process', () => {
+    // given
+    const bfInterpreter = new BFInterpreter();
+    const sourceModel = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<+" +
+        "+.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+."
+    bfInterpreter.environment.setProperty(Environment.SOURCE_MODEL, sourceModel);
+    // when
+    bfInterpreter.process()
+    // then
+    expect(bfInterpreter.environment.getResult()).toBe("Hello, World!")
+});
