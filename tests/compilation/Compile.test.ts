736efd71d133e5b72bf0a4bdc1b633399644f344
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-04-26 18:05:44 +0200

    Add test case for compileProcessesorAndReturnResult

--- tests/compilation/Compile.test.ts
+++ tests/compilation/Compile.test.ts
@@ -1,11 +1,21 @@
-import { createCompilationContextFromProcessors, Identity } from "../../src";
+import {compileProcessesorAndReturnResult, createCompilationContextFromProcessors, Identity} from "../../src";
 
-test('createCompilationContextFromProcessors', () => {
+test("createCompilationContextFromProcessors", () => {
     //given
-    let sourceModel = "sourceModel";
+    const sourceModel = "sourceModel";
     //when
     const context = createCompilationContextFromProcessors(sourceModel, Identity);
-    context.compile()
+    context.compile();
     //then
-    expect(context.getResult()).toBe(sourceModel)
+    expect(context.getResult()).toBe(sourceModel);
+});
+
+test("compileProcessesorAndReturnResult", () => {
+    //given
+    const expectedSourceModel = "sourceModel";
+    const properties = {foo: "bar"};
+    //when
+    const actualSourceModel = compileProcessesorAndReturnResult(expectedSourceModel, properties, Identity);
+    //then
+    expect(actualSourceModel).toBe(expectedSourceModel);
 });
